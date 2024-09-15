e1b7a5fbcf2bf905b431ea51f5bbaaa4b57c8689
marcfranciss <marcfrancissurban@gmail.com>
2024-09-15 21:50:17 +0800

    tablet done

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -4,6 +4,8 @@ import closeBtn from "../../assets/images/icon-menu-close.svg";
 import "./navbar.sass";
 import { useState } from "react";
 
+const navArr = ["home", "new", "popular", "trending", "categories"];
+
 const NavBar = () => {
   const [isCollapse, setIsCollapse] = useState<boolean>(false);
   return (
@@ -12,10 +14,23 @@ const NavBar = () => {
         <div className='logo-container'>
           <img src={navLogo} alt='' />
         </div>
+        <div className='main-nav'>
+          <ul>
+            {navArr.map((arr) => {
+              return (
+                <li key={arr}>
+                  <a href={`#${arr}`} className='text-body'>
+                    {arr}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <button
           type='button'
           className='navbar-toggler'
-          aria-controls='main-nav'
+          aria-controls='mobile-nav'
           aria-expanded={isCollapse}
           aria-label='Toggle navigation'
           onClick={() => setIsCollapse(!isCollapse)}>
@@ -23,7 +38,7 @@ const NavBar = () => {
         </button>
       </div>
       <dialog
-        id='main-nav'
+        id='mobile-nav'
         className={`links-container  ${isCollapse && "collapse"}`}
         aria-live='polite'
         aria-atomic={isCollapse}>
@@ -33,12 +48,12 @@ const NavBar = () => {
             className='close-nav'
             onClick={() => setIsCollapse(false)}
             style={{ alignSelf: "flex-end" }}
-            aria-controls='main-nav'
+            aria-controls='mobile-nav'
             aria-expanded={isCollapse}
             aria-label='Close navigation'>
             <img src={closeBtn} alt='' />
           </button>
-          {["home", "new", "popular", "trending", "categories"].map((arr) => {
+          {navArr.map((arr) => {
             return (
               <li key={arr}>
                 <a href={`#${arr}`} className='heading-xs'>
